10dcd235dab28328c79463d404d72f66821adb26
Bryan Jackson <bjack13@gmail.com>
2017-06-08 11:50:49 -0600

    showing form module and setting up form Group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AlertModule.forRoot(),
+    ReactiveFormsModule
+  ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
-  ],
-  imports: [
-    BrowserModule
+    HeroDetailComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl }       from '@angular/forms';
+import { Component} from '@angular/core';
+import { FormControl, FormGroup }       from '@angular/forms';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent {
   name = new FormControl();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  heroForm = new FormGroup ({
+    name: new FormControl()
+  });
 
 }
